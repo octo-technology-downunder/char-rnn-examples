40206f42f96b267231829895d86f251774ec3e36
Erwan Alliaume <ealliaume@gmail.com>
2015-12-03 03:48:52 +0000

    startups

---
+++ run-startups-gpu.sh
@@ -0,0 +1,1 @@
+th train.lua -data_dir ./data/startups -rnn_size 512 -num_layers 2 -dropout 0.5 
---
+++ run-startups-sampling-gpu.sh
@@ -0,0 +1,5 @@
+
+CHECKPOINT=./cv_tokeep/startups_lm_lstm_epoch50.00_3.0370.t7
+
+
+th sample.lua $CHECKPOINT
